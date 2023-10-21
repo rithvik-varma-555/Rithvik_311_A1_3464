539e0c1abd39677c83265d89766f767f33721726
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-10-21 19:50:09 -0400

    input for is point inside

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ class Main {
         Triangle[] triangles = new Triangle[numOfTriangles];
 
         for (int i = 0; i < numOfTriangles; i++) {
-            System.out.println("Enter the coordinates of the triangle 1" + (i + 1));
+            System.out.println("Enter the coordinates of the triangle " + (i + 1));
             Point[] v = new Point[3];
 
             for (int j = 0; j < 3; j++) {
@@ -33,6 +33,11 @@ class Main {
             double y = input.nextDouble();
             Point point = new Point(x, y);
 
+            if (triangle.isPointInside(point)) {
+                System.out.println("The point is inside the triangle.");
+            } else {
+                System.out.println("The point is outside the triangle");
+            }
         }
 
     }
