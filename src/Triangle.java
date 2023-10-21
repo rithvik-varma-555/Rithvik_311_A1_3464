b98b02ea53d892aeb9e1e2d86d543a82b6d386a0
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-10-21 19:20:06 -0400

    created perimeter method

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,3 +1,24 @@
 class Triangle {
+   Point[] v = new Point[3];
+
+   public Triangle(Point p1, Point p2, Point p3) {
+       v[0] = p1;
+       v[1] = p2;
+       v[2] = p3;
+   }
+
+   public double calculatePerimeter() {
+       double s1 = v[0].distance(v[1]);
+       double s2 = v[1].distance(v[2]);
+       double s3 = v[2].distance(v[0]);
+       return s1+s2+s3;
+   }
+
+   public double calculateArea() {
+
+   }
+
+
+
 
 }
