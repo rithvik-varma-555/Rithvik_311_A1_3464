8023a547349ca6aabc683e43697ea58507090bd3
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-10-21 19:23:43 -0400

    created calculateArea()

--- src/Triangle.java
+++ src/Triangle.java
@@ -15,7 +15,11 @@ class Triangle {
    }
 
    public double calculateArea() {
-
+       double s1 = v[0].distance(v[1]);
+       double s2 = v[1].distance(v[2]);
+       double s3 = v[2].distance(v[0]);
+       double s = (s1 + s2 +s3) / 2;
+       return  Math.sqrt(s * (s - s1) * (s - s2) * (s - s3));
    }
 
 
