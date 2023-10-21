ad85f9f826791f3fb9e6ed2d8dbb17855a0b69c7
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-10-21 19:47:33 -0400

    input for is point inside

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,29 @@ class Main {
 
         for (int i = 0; i < numOfTriangles; i++) {
             System.out.println("Enter the coordinates of the triangle 1" + (i + 1));
-            Point[] vertices = new Point[3];
+            Point[] v = new Point[3];
 
             for (int j = 0; j < 3; j++) {
                 System.out.println("Vertix " + (j + 1) + " X and Y: ");
                 double x = input.nextDouble();
                 double y = input.nextDouble();
-                vertices[j] = new Point(x, y);
+                v[j] = new Point(x, y);
             }
 
-            triangles[i] = new Triangle(vertices[0], vertices[1], vertices[2]);
+            triangles[i] = new Triangle(v[0], v[1], v[2]);
+        }
+
+        for (int i = 0; i < numOfTriangles; i++) {
+            Triangle triangle = triangles[i];
+            double perimeter = triangle.calculatePerimeter();
+            double area = triangle.calculateArea();
+            String classification = triangle.classifyTriangle();
+
+            System.out.println("Triangle "+ (i + 1) + " , Perimeter: "+ perimeter + " , Area: "+area+" ,Type: " + classification);
+            double x = input.nextDouble();
+            double y = input.nextDouble();
+            Point point = new Point(x, y);
+
         }
 
     }
