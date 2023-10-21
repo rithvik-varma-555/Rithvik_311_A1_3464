117701fe3fe94126962b9d96c3dc925d31da9e7c
Rithvik Varma <rithvikvarmaa@gmail.com>
2023-10-21 19:14:33 -0400

    added new input

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,23 @@ class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter the number of triangles: ");
-        int numOfTriangles - input.nextInt();
+        int numOfTriangles = input.nextInt();
 
-        Triangle[] triangle = new Triangle[numOfTriangles];
+        Triangle[] triangles = new Triangle[numOfTriangles];
 
         for (int i = 0; i < numOfTriangles; i++) {
             System.out.println("Enter the coordinates of the triangle 1" + (i + 1));
             Point[] vertices = new Point[3];
 
             for (int j = 0; j < 3; j++) {
-
+                System.out.println("Vertix " + (j + 1) + " X and Y: ");
+                double x = input.nextDouble();
+                double y = input.nextDouble();
+                vertices[j] = new Point(x, y);
             }
+
+            triangles[i] = new Triangle(vertices[0], vertices[1], vertices[2]);
         }
+
     }
 }
